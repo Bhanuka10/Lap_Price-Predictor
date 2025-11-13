895293e1bdb1bd77e83da3628f7909fcbaa7540c
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-11-14 00:53:43 +0530

    display form details

--- Website/app.py
+++ Website/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='Template')
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == 'POST':
+        ram = request.form['Ram']
+        Weight = request.form['Weight']
+        Touchscreen = request.form['Touchscreen']
+        IPS = request.form['IPS']
+        Companey = request.form['Companey']
+        TypeName = request.form['TypeName']
+        OperatingSystem = request.form['OperatingSystem']
+        cpu= request.form['cpu']
+        gpu = request.form['gpu']
+        print(ram, Weight, Touchscreen, IPS, Companey, TypeName, OperatingSystem, cpu, gpu)
     return render_template('index.html')
 
 if __name__ == '__main__':
